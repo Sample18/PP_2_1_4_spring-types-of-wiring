6ea4d50a37571e0abbdf418ec348859ee37862de
Sample18 <Sample417@yandex.ru>
2024-03-16 17:01:19 +0300

    all beans bounded

--- src/main/java/koschei/config/AppConfig.java
+++ src/main/java/koschei/config/AppConfig.java
@@ -1,7 +1,10 @@
 package koschei.config;
 
+import koschei.models.Deth8;
 import koschei.models.Island2;
+import koschei.models.Needle7;
 import koschei.models.Wood3;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -14,4 +17,7 @@ public class AppConfig {
     public static Island2 getIsland(Wood3 wood) {
         return new Island2(wood);
     }
+
+    @Bean
+    public static Needle7 getNeedle(Deth8 deth) {return new Needle7(deth);}
 }
